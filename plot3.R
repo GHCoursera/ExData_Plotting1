f99ce3a7a0e914d37be084a6611566c83c453d68
GHCoursera <noltingm@t-online.de>
2016-11-19 19:26:39 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,9 +8,9 @@ data1<-subset(data, as.Date(Date,format='%d/%m/%Y')>= '2007-02-01' & as.Date(Dat
 plot3<-plot(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_1), type = 'n',
       xlab=' ',ylab='Energy sub metering')
       lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_1), col = 'black')
-      lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_2), col = 'blue')
-      lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_3), col='red')
-      legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "blue", "red"))
+      lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_2), col = 'red')
+      lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_3), col='blue')
+      legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
       
 dev.copy(png,file="plot3.png", width=480, height=480)
 dev.off()
