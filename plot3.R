0da5d607e7c3d2094355c7946169f04bc5b96584
GHCoursera <noltingm@t-online.de>
2016-11-19 19:22:53 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -10,7 +10,7 @@ plot3<-plot(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.
       lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_1), col = 'black')
       lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_2), col = 'blue')
       lines(strptime(paste(data1$Date,data1$Time),format='%d/%m/%Y%H:%M:%S'),as.numeric(data1$Sub_metering_3), col='red')
-      legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+      legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "blue", "red"))
       
 dev.copy(png,file="plot3.png", width=480, height=480)
 dev.off()
